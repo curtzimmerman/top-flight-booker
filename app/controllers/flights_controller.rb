d89e4d32b051b504a56c62f4dfa8956a6e14e2de
Curt Zimmerman <czimmerman9@gmail.com>
2015-06-08 15:55:32 -0400

    add search function

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,12 @@ class FlightsController < ApplicationController
 		@start_locations = Airport.all.map { |a| [a.code, a.id]}
 		@destinations = Airport.all.map { |a| [a.code, a.id]}
 		@departure_times = @flights.all.order("departure_time ASC")
+		@num_passengers = (1..4).map { |n| [n,n] }
+
+		@results = Flight.search(params[:search])
 	end
+
+	private
+
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -8,5 +8,12 @@ class Flight < ActiveRecord::Base
 	def departure_time_formatted
 		self.departure_time.strftime("%m/%d/%Y")
 	end
+
+	def self.search(params)
+		if params
+			Flight.where(
+								"start_location_id = ? AND destination_id = ? AND departure_time BETWEEN ? AND ?", params[:start_location], params[:destination], params[:departure_time].to_date.beginning_of_day, params["departure_time"].to_date.end_of_day)
+		end
+	end
 end
 
